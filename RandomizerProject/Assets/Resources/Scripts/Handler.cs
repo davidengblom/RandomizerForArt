c0386f4d78db714a6f9236b2063aba2fdf1039f8
David Engblom <56073597+davidengblom@users.noreply.github.com>
2020-10-29 16:38:39 +0100

    Update

---
+++ RandomizerProject/Assets/Resources/Scripts/Handler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Resources.Scripts
+{
+    public class Handler : MonoBehaviour
+    {
+        public List<string> categories = new List<string>();
+        public Dropdown[] dropDowns;
+
+        public Text text;
+
+        private void Start()
+        {
+            Debug.Log(Application.streamingAssetsPath + "/JSON/Categories.json");
+        }
+    }
+}
---
+++ RandomizerProject/Assets/Resources/Scripts/JsonReader.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace Resources.Scripts
+{
+    public class JsonReader : MonoBehaviour
+    {
+        [SerializeField] private Handler handler;
+        public TextAsset categoriesFile;
+
+        private void Start()
+        {
+            categoriesFile = (TextAsset) UnityEngine.Resources.Load(Application.streamingAssetsPath + "/JSON/Categories.json");
+            var categoriesInJson = JsonUtility.FromJson<Categories>(categoriesFile.text);
+
+            foreach (var category in categoriesInJson.categories)
+            {
+                handler.categories.Add(category);
+            }
+
+            foreach (var dropdown in handler.dropDowns)
+            {
+                dropdown.AddOptions(handler.categories);
+            }
+        }
+    }
+}
